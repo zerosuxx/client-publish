85d5ecaea2b9496c5c398df0cf704bc8ea517014
szenti <mszentivanyi@emarsys.com>
2017-01-12 16:08:08 +0100

    feat(client-publish): convert to ES6

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 'use strict';
 
-let s3 = require('./lib/s3');
+let S3 = require('./lib/s3');
 let Redirector = require('./lib/redirector');
 let Revision = require('./lib/revision');
-const REVISION_TYPE = 'timestamp';
 
 module.exports.deploy = function() {
-  let revision = Revision.get(REVISION_TYPE);
+  let revision = Revision.get(Revision.REVISION_TYPE_TIMESTAMP);
+  console.log(`Deploying revision ${revision}`);
 
-  s3.publish(revision)
+  S3.publish(revision)
     .then(() => {
-      console.log('Deploy successful');
+      console.log('Deployment successful');
     })
     .catch((err) => {
-      console.log('Error: ', err.message);
+      console.log('Error while deploying: ', err.message);
     });
 
-  return Redirector.save(REVISION_TYPE);
+  return Redirector.save(Revision.REVISION_TYPE_TIMESTAMP);
 };
 
-module.exports.lib = {
-  revision: require('./lib/revision')
-};
+module.exports.lib = Revision;
