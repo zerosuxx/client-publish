e9e689f2720fdacbceeeb573757e923a469cbb10
szenti <mszentivanyi@emarsys.com>
2017-01-13 11:59:39 +0100

    refactor(s3-wrapper): rename S3 to S3Wrapper

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let S3 = require('./lib/s3');
+let S3Wrapper = require('./lib/s3-wrapper');
 let Redirector = require('./lib/redirector');
 let Revision = require('./lib/revision');
 
@@ -8,7 +8,7 @@ module.exports.deploy = function() {
   let revision = Revision.get(Revision.REVISION_TYPE_TIMESTAMP);
   console.log(`Deploying revision ${revision}`);
 
-  S3.publish(revision)
+  S3Wrapper.publish(revision)
     .then(() => {
       console.log('Deployment successful');
     })
