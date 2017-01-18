773253bc8d6b5f57473765686a381a0abc39bd96
blacksonic <soos.gabor86@gmail.com>
2017-01-18 16:18:10 +0100

    feat(deploy): merge to production

--- index.js
+++ index.js
@@ -3,15 +3,17 @@
 let S3Wrapper = require('./lib/s3-wrapper');
 let Redirector = require('./lib/redirector');
 let Revision = require('./lib/revision');
+let Merge = require('./lib/merge');
 let argv = require('yargs').argv;
+let inquirer = require('inquirer');
 
-module.exports.deploy = function() {
+module.exports.publish = function() {
   let revision = Revision.get(Revision.REVISION_TYPE_TIMESTAMP, argv.revision);
-  console.log(`> Deploying with revision ${revision}`);
+  console.log(`> Publishing with revision ${revision}`);
 
   S3Wrapper.publish(revision)
     .then(() => {
-      console.log('> S3 deployment successful');
+      console.log('> S3 upload successful');
       console.log('> Updating redirector');
       return Redirector.save(revision);
     })
@@ -19,7 +21,31 @@ module.exports.deploy = function() {
       console.log('> Redirector update successful');
     })
     .catch((err) => {
-      console.log('> Error while deploying');
+      console.log('> Error while publishing');
       console.log(err);
     });
+};
+
+module.exports.deploy = function() {
+  let question = {
+    type: 'confirm',
+    name: 'deploy',
+    message: 'Do you really want to deploy to production?',
+    default: false
+  };
+
+  inquirer.prompt([question]).then(function(answers) {
+    if (answers.deploy) {
+      console.log('> Merging to production branch');
+      Merge.toProduction()
+        .then(function(result) {
+          console.log(result);
+          process.exit(result.exit);
+        })
+        .catch(function(err) {
+          console.log(err);
+          process.exit(1);
+        });
+    }
+  });
 };
