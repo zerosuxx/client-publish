db13d93afab17608d9935a2569717c352dede1c7
Marton Visnovitz <marton.visnovitz@emarsys.com>
2022-01-14 15:49:40 +0100

    feat(revision): add the option to get the next revision
    EME-3628
    
    Co-authored-by: David Kocsis <david.kocsis@emarsys.com>

--- bin/client-publish-revision.js
+++ bin/client-publish-revision.js
@@ -13,23 +13,21 @@ const modeList = [
 
 program
   .option('-m --mode <mode>', `the way to get the revision (${modeList.join('|')})`, 'timestamp')
+  .option('-n --next', 'get the next revision based on conventional commit messages')
   .parse(process.argv);
 
 const revision = async (program) => {
   const options = program.opts();
   const mode = options.mode;
+  const next = options.next;
 
-  const revision = getCurrentRevision(mode);
-  console.log(revision);
-};
-
-const getCurrentRevision = (mode) => {
   if (!modeList.includes(mode)) {
     console.log(`! Invalid mode "${mode}". Mode must be (${modeList.join('|')}). Exiting.`);
     process.exit(1);
   }
 
-  return Revision.get(mode);
+  const revision = next ? await Revision.getNext(mode) : Revision.get(mode);
+  console.log(revision);
 };
 
 revision(program);
