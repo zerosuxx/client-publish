58c1f6f7d05d6cc5336bcc94a983ba63e87dff59
Marton Visnovitz <marton.visnovitz@emarsys.com>
2022-01-14 15:52:30 +0100

    feat(tag): option to add prefix to version
    EME-3628
    
    Co-authored-by: David Kocsis <david.kocsis@emarsys.com>

--- bin/client-publish-tag.js
+++ bin/client-publish-tag.js
@@ -8,23 +8,26 @@ const createTag = require('../lib/git/tag');
 
 program
   .option('-r --revision [revision]', 'the revision to use (default: PROJECT_REVISION)')
+  .option('-p --prefix [prefix]', 'the prefix to add to the version number')
   .option('-y --yes', 'automatic yes to prompt', false)
   .parse(process.argv);
 
 const tag = async (program) => {
   const options = program.opts();
   const revision = options.revision || Revision.get(Revision.REVISION_TYPE_ENV);
+  const prefix = options.prefix || '';
   const autoYes = options.yes;
+  const newTag = `${prefix}${revision}`;
 
   if (autoYes) {
-    await createTag(revision);
+    await createTag(newTag);
     return;
   }
 
   const question = {
     type: 'confirm',
     name: 'tag',
-    message: `Do you really want to create a new git tag for revision ${revision}?`,
+    message: `Do you really want to create a new git tag ${newTag}?`,
     default: false
   };
 
@@ -34,7 +37,7 @@ const tag = async (program) => {
     process.exit(1);
   }
 
-  await createTag(revision);
+  await createTag(newTag);
 };
 
 tag(program);
