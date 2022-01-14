749b22db54efc7c477d5bfb68c59b340a681e9e0
Marton Visnovitz <marton.visnovitz@emarsys.com>
2022-01-14 15:06:41 +0100

    feat(revision): add git tag revision method
    EME-3628
    
    Co-authored-by: David Kocsis <david.kocsis@emarsys.com>

--- bin/client-publish-revision.js
+++ bin/client-publish-revision.js
@@ -4,27 +4,32 @@
 const program = require('commander');
 const Revision = require('../lib/utils/revision');
 
+const modeList = [
+  Revision.REVISION_TYPE_ENV, // env
+  Revision.REVISION_TYPE_PACKAGE, // package
+  Revision.REVISION_TYPE_TIMESTAMP, // timestamp
+  Revision.REVISION_TYPE_GIT_TAG // git-tag
+];
+
 program
-  .option('-m --mode <mode>', 'the way to get the revision (env|package|timestamp)', 'timestamp')
+  .option('-m --mode <mode>', `the way to get the revision (${modeList.join('|')})`, 'timestamp')
   .parse(process.argv);
 
 const revision = async (program) => {
   const options = program.opts();
   const mode = options.mode;
 
-  const modeMap = {
-    env: Revision.REVISION_TYPE_ENV,
-    package: Revision.REVISION_TYPE_PACKAGE,
-    timestamp: Revision.REVISION_TYPE_TIMESTAMP
-  };
+  const revision = getCurrentRevision(mode);
+  console.log(revision);
+};
 
-  if (!Object.keys(modeMap).includes(mode)) {
-    console.log(`! Invalid mode "${mode}". Mode must be (env|package|timestamp). Exiting.`);
+const getCurrentRevision = (mode) => {
+  if (!modeList.includes(mode)) {
+    console.log(`! Invalid mode "${mode}". Mode must be (${modeList.join('|')}). Exiting.`);
     process.exit(1);
   }
 
-  const revision = Revision.get(modeMap[mode]);
-  console.log(revision);
+  return Revision.get(mode);
 };
 
 revision(program);
